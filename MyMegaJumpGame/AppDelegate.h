c511257c3d7378d615db12666c54c57a15fec38f
Bo Guan <Bob@Bobs-Macbook-pro-Retina.local>
2015-02-02 12:58:56 -0500

    Initial Commit

---
+++ MyMegaJumpGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMegaJumpGame
+//
+//  Created by Bo Guan on 2/2/15.
+//  Copyright (c) 2015 Bo Guan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMegaJumpGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MyMegaJumpGame
+//
+
+//  Copyright (c) 2015 Bo Guan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
